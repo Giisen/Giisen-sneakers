717d9a09fc3264473e20916a8bea34c7c9f743d1
giisen <kristerekelund@me.com>
2023-02-01 16:40:59 +0100

    modal funkar

--- Javascript/webshop2.js
+++ Javascript/webshop2.js
@@ -45,32 +45,60 @@ let totalItemsInCart = () => {
 };
 totalItemsInCart();
 
-// const buttons = document.getElementsByTagName("button");
-// const result = document.getElementById("result");
+const lista = document.getElementById("listaprodukter");
 
-// const buttonPressed = (e) => {
-//   console.log(e.target.id);
-// };
+for (const produkter of shopItemsData) {
+  const node = document.createElement("li");
+  node.onclick = () => {
+    getItemInfo(produkter.id);
+  };
+  const textnode = document.createTextNode(produkter.name);
+  node.appendChild(textnode);
 
-// for (let button of buttons) {
-//   button.addEventListener("click", buttonPressed);
-// }
+  document.getElementById("listaprodukter").appendChild(node);
+  console.log(produkter.name);
+}
+
+function getItemInfo(id) {
+  console.log(id);
+  const search = shopItemsData.find((x) => x.id === id) || [];
+  console.log(search.price);
+  const modalText = document.getElementById("modal-text");
+  modalText.innerHTML = `
+      <div id=product-id-${search.id} class="item">
+
+        <div class="details">
+          <h3>${search.name}</h3>
+          <p>${search.decription}</p>
+        </div>
+
+        <div class="price-quantity">
+                <h2>${search.price} kr</h2>
+        </div>
+
+        <div class="buttons">
+          <i onclick="removeOneItem(${search.id})" class="bi bi-dash-lg"></i>
+
+          <div id=${search.id} class="quantity">
+            ${search.item === undefined ? 0 : search.item}
+          </div>
+
+          <i onclick="addOneItem(${search.id})" class="bi bi-plus-lg"></i>
+
+        </div>
+
+      </div>
+    `;
+
+  document.getElementById("myModal").style.display = "block";
+}
 
 const modal = document.getElementById("myModal");
-
-const btn = document.getElementById("sneaker1");
 
 // Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-  selectedId = btn.id;
-  console.log("selectedid......." + selectedId);
-};
-
-// When the user clicks on <span> (x), close the modal
+//When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = "none";
 };
@@ -81,38 +109,3 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
-
-function getItemInfo() {
-  const search = shopItemsData.find((x) => x.id === "sneaker1") || [];
-
-  return `
-      <div id=product-id-${search.id} class="item">
-        
-        <div class="details">
-          <h3>${search.name}</h3>
-          <p>${search.decription}</p>
-        </div>
-        
-        
-        <div class="price-quantity">
-                <h2>${search.price} kr</h2>
-        </div>
-
-
-        <div class="buttons">
-          <i onclick="removeOneItem(${search.id})" class="bi bi-dash-lg"></i>
-          
-          <div id=${search.id} class="quantity">
-            ${search.item === undefined ? 0 : search.item}
-          </div>
-          
-          <i onclick="addOneItem(${search.id})" class="bi bi-plus-lg"></i>
-          
-        </div>
-      
-      
-      </div>
-    `;
-}
-
-document.getElementById("modal-text").innerHTML = getItemInfo();
